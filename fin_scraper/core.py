11b7cc9065a9d90b0a45e672d577e2b3fa2d167f
Ryan <ryanafree@gmail.com>
2016-09-18 18:48:21 -0500

    condense core file

--- fin_scraper/core.py
+++ fin_scraper/core.py
@@ -1,12 +1,17 @@
 import requests
-import bs4
+import csv
 
+
+fhand = open('exchange_ticker_pairs.csv')
+fread = csv.reader(fhand)
+fdata = list(fread)
+
+'''
 current_data = requests.get('http://www.morningstar.com/stocks/xnas/msft/quote.html')
 prior_income_statement = requests.get('http://financials.morningstar.com/income-statement/is.html?t=MSFT&region=USA&culture=en_US')
 prior_balance_sheet = requests.get('http://financials.morningstar.com/balance-sheet/bs.html?t=MSFT&region=usa&culture=en-US')
 prior_cash_flow = requests.get('http://financials.morningstar.com/cash-flow/cf.html?t=MSFT&region=usa&culture=en-US')
-
-
+'''
 # This is the page tag for last price
 # //*[@id="last-price-value"]
 
